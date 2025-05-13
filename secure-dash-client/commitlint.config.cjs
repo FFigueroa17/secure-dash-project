fba5a5a33b6d8bb772268f9a3be19450b5570d22
FFigueroa17 <00019921@uca.edu.sv>
2025-05-12 23:49:18 -0600

    chore(config): :wrench: adds commitlint gitmoji support
    Enables gitmoji support for commitlint to encourage more expressive and consistent commit messages.

--- secure-dash-client/commitlint.config.cjs
+++ secure-dash-client/commitlint.config.cjs
@@ -1,1 +1,16 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+// commitlint.config.cjs
+/** @type {import('commitlint').UserConfig} */
+module.exports = {
+  // conventional types  +  gitmoji check
+  extends: ['@commitlint/config-conventional', 'gitmoji'],
+  plugins: ['commitlint-plugin-gitmoji'],
+
+  parserPreset: {
+    // <type>(optional-scope): <emoji> <subject>
+    parserOpts: {
+      headerPattern:
+        /^(\w+)(?:\(([^)]+)\))?:\s(:\S+?:)\s(.+)$/u,
+      headerCorrespondence: ['type', 'scope', 'emoji', 'subject'],
+    },
+  },
+};
