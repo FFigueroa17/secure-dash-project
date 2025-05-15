c8c9fb57e45869304114cbfc9ee63ddfb4a52686
FFigueroa17 <00019921@uca.edu.sv>
2025-05-15 10:08:49 -0600

    chore(config): :wrench: refine commitlint header pattern
    Updated the header pattern in commitlint configuration for improved parsing consistency.

--- secure-dash-client/commitlint.config.cjs
+++ secure-dash-client/commitlint.config.cjs
@@ -1,5 +1,3 @@
-// commitlint.config.cjs
-/** @type {import('commitlint').UserConfig} */
 module.exports = {
   // conventional types  +  gitmoji check
   extends: ['@commitlint/config-conventional', 'gitmoji'],
@@ -8,8 +6,7 @@ module.exports = {
   parserPreset: {
     // <type>(optional-scope): <emoji> <subject>
     parserOpts: {
-      headerPattern:
-        /^(\w+)(?:\(([^)]+)\))?:\s(:\S+?:)\s(.+)$/u,
+      headerPattern: /^(\w+)(?:\(([^)]+)\))?:\s(:\S+?:)\s(.+)$/u,
       headerCorrespondence: ['type', 'scope', 'emoji', 'subject'],
     },
   },
